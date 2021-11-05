8aa6b5f091b76c7fe404c050858b66692ff147ab
unknown <Unsys@HAM-ITS0886.coremedia.com>
2021-11-06 00:04:53 +0100

    init changes

--- unsys/__init__.py
+++ unsys/__init__.py
@@ -3,7 +3,8 @@ from itertools import combinations
 import numpy as np
 import sympy as sp
 import sympy.physics.quantum as spq
-import string
+import hypernetx as hnx
+from hnxwidget import HypernetxWidget
 
 # global variables
 guid = 0
@@ -14,56 +15,36 @@ def getUID(prefix="id#"):
     guid = guid + 1
     return prefix + str(guid - 1)
 
-# qudit is a unique label identifying the qudit system the state belongs to
-class BasisState:
-    def __init__(self, qudit, ket=spq.Ket('0'), amplitude=1):
-        self.qudit = qudit
-        self.ket = ket
-        self.amplitude = amplitude
-
-#pure hanges the interpretation of qubits with multiple states
-class StateCorrelation:
-    def __init__(self, weight):
-        self.uid = getUID("c#")
-        self.state_map = {}
-    
-    #s is a Basis State
-    def addState(self, s):
-        if (s.qudit not in self.state_map.keys()):
-            self.state_map[s.qudit] = [] 
-  
-        self.state_map[s.qudit].append(s)
-
 class QuditSystem:
     def __init__(self, nb_qudits, dim, symbolic= False):
         global guid
 
         self.dim = dim
-        self.qudits = {}
-        self.correlations = {}
         self.nb_qudits = nb_qudits
 
         #init qudits
-        for i in range(0, nb_qudits):
-            qudit_label = "q" + str(i)
-            self.qudits[qudit_label] = []
+        qudits = []
 
-        for qudit in self.qudits:
+        for qudit in range(nb_qudits):
             if (symbolic):
                 j = 0
+                state = 0
                 while j < dim:
-                    amplitude = sp.symbols(qudit_label+'_'+str(j))
-                    state = BasisState(qudit, spq.Ket(j), amplitude)
-
-                    self.qudits[qudit].append(state)
+                    coeff = sp.symbols('coeff_'+str(j))
+                    state += coeff*spq.Ket(j)
                     
                     j += 1
+                qudits.append(hnx.Entity(getUID(),props={'qudit':qudit, 'state':state}))
             else:
-                q = BasisState(qudit)
-                self.qudits[qudit].append(q)
+                qudits.append(hnx.Entity(getUID(),props={'qudit':qudit, 'state':spq.Ket(0)}))
         
-    def solveInterference():
-        pass
+        #Initialize Hypergraph
+        self.hypergraph = hnx.Hypergraph({
+            getUID('system#'): qudits
+        })
+        
+    def draw(self):
+        hnx.draw(self.hypergraph)
 
-    def solveEntanglement():
-        pass
+    def interactive(self):
+        HypernetxWidget(self.hypergraph)
