607e7a68d4897bb4b3a4ce9fff694abff02496f9
dncolomer <uncertainsystems@gmail.com>
2021-11-18 02:35:48 +0100

    cascade node removal

--- unsys/__init__.py
+++ unsys/__init__.py
@@ -16,31 +16,34 @@ def getUID(prefix="id#"):
     return prefix + str(guid - 1)
 
 class QuditSystem:
-    def __init__(self, nb_qudits, dim, symbolic= False):
+    def __init__(self, nb_qudits, dim, symbolic= False, hypergraph= None):
         global guid
 
         self.dim = dim
         self.nb_qudits = nb_qudits
 
-        #init qudits
-        self.qudits = []
+        if (hypergraph is not None):
+            self.hypergraph = hypergraph
+        else:
+            #init qudits
+            qudits = []
 
-        for qudit in range(nb_qudits):
-            if (symbolic):
-                j = 0
-                state = 0
-                while j < dim:
-                    coeff = sp.symbols('coeff_'+str(j))
-                    state += coeff*spq.Ket(j)
-                    
-                    j += 1
-                self.qudits.append(hnx.Entity(getUID(),props={'qudit':qudit, 'state':state}))
-            else:
-                self.qudits.append(hnx.Entity(getUID(),props={'qudit':qudit, 'state':spq.Ket(0)}))
-        
-        #Initialize Hypergraph
-        syslabel = getUID('system#')
-        self.hypergraph = hnx.Hypergraph({syslabel: hnx.Entity(syslabel,elements=self.qudits,props={'coeff':1})})
+            for qudit in range(nb_qudits):
+                if (symbolic):
+                    j = 0
+                    state = 0
+                    while j < dim:
+                        coeff = sp.symbols('coeff_'+str(j))
+                        state += coeff*spq.Ket(j)
+                        
+                        j += 1
+                    qudits.append(hnx.Entity(getUID(),props={'qudit':qudit, 'state':state}))
+                else:
+                    qudits.append(hnx.Entity(getUID(),props={'qudit':qudit, 'state':spq.Ket(0)}))
+            
+            #Initialize Hypergraph
+            syslabel = getUID('system#')
+            self.hypergraph = hnx.Hypergraph({syslabel: hnx.Entity(syslabel,elements=qudits,props={'coeff':1})})
 
     def stateIntersection(self,state1,state2):
         s1_symbols = state1.free_symbols
@@ -72,34 +75,33 @@ class QuditSystem:
 
     def getQuditNodes(self,qudit):
         node_list = []
-        for e in self.qudits:
+        for e in self.hypergraph.nodes():
             if (e.props['qudit'] == qudit):
                 node_list.append(e.uid)
         
         return node_list
 
+    def cascadeNodeRemoval(self,hedges):
+        return self.hypergraph
+
     # subsystems = list of qudits
     def postSelect(self,qudit,state):
         hg = self.hypergraph
         #delete all nodes where states don't overlap (share no computational basis elements)
-        for e in self.qudits:
+        for e in self.hypergraph.nodes():
             state_intersection = self.stateIntersection(state,e.props['state'])
-            if (e.props['qudit'] == qudit and len(state_intersection) == 0):
-                #fetch all the hyperedges this node belongs to
-                hedges = e.memberships
+            if (e.props['qudit'] == qudit):
+                if (len(state_intersection) == 0):
+                    #fetch all the hyperedges this node belongs to
+                    hedges = e.memberships
 
-                #remove node from hypergraph
-                hg = self.hypergraph.remove_node(e.uid)
+                    #remove node from hypergraph
+                    self.hypergraph = self.hypergraph.remove_node(e.uid)
 
-                #for hedge in hedges: 
-                    #for all the other remaining nodes
-                        #if not belong to another hedge that's not in the list to process
-                            #delete noded
-                        #if edge empty
-                            #delete edge
-                
-
-            else:
-                #we colapse the noded state to <state>
-                #REVIEW
-                e.props['state'] = state        
+                    #update the rest of the hypergraph
+                    self.cascadeNodeRemoval(hedges)
+                    
+                else:
+                    #we colapse the noded state to <state>
+                    #REVIEW
+                    e.props['state'] = state        
